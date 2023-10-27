2e4d6e407776a024128278eaeb319c69bde677d7
yu <yu@sec.com>
2023-10-27 22:29:19 +0800

    修改搜索功能

--- Test删除星球.mjs
+++ Test删除星球.mjs
@@ -8,11 +8,20 @@ const saveData = loadFile(fileName);
  
 
 const ass=saveData.gameObjects.find(x=>x.name==="Asteroid");//  
-for(const seed of ass.gameObjects){
-    const asskey=seed.behaviors.find(x=>x.name=== "AsteroidGridEntity")  //找名字
-    console.log(  asskey.templateData.m_name )
+for(let i=0;i<ass.gameObjects.length;i++){
+     
+    const seed=ass.gameObjects[i];
+    const name=seed.behaviors.find(x=>x.name=="AsteroidGridEntity").templateData.m_name;
+    if(name==="delete"){
+        //删除
+        ass.gameObjects.splice(i,1);
+        console.log( "删除了" )
+    }else{
+        console.log( name )
+    }
+    i--;
 }
- 
+saveFile("删除asteroid.sav");
 console.log("完成")
 
 
